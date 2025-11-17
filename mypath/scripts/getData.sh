f2b504acad37e5603bf780bc3dedd8abbb8e38fd
touhoufan <touhoufan2024@gmail.com>
2025-11-17 12:49:43 +0800

    获取数据 镜像加速
    Signed-off-by: touhoufan <touhoufan2024@gmail.com>

--- mypath/scripts/getData.sh
+++ mypath/scripts/getData.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -e use_proxy=yes -e https_proxy=127.0.0.1:10808 https://github.com/chenditc/investment_data/releases/latest/download/qlib_bin.tar.gz -O /tmp/qlib_bin.tar.gz
-mv  ~/.qlib/qlib_data/cn_data  ~/.qlib/qlib_data/cn_data_back
+wget "https://hk.gh-proxy.org/https://github.com/chenditc/investment_data/releases/latest/download/qlib_bin.tar.gz" -O /tmp/qlib_bin.tar.gz
+mv  ~/.qlib/qlib_data/cn_data  ~/.qlib/qlib_data/cn_data_back_$(date +%Y%m%d_%H%M%S)
 mkdir -p ~/.qlib/qlib_data/cn_data
 tar -zxvf /tmp/qlib_bin.tar.gz -C ~/.qlib/qlib_data/cn_data --strip-components=1
