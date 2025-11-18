11bf0ab381ff21749320ee5f1fa3e5e666305e44
touhoufan <touhoufan2024@gmail.com>
2025-11-18 10:48:49 +0800

    getdata: no proxy wget
    Signed-off-by: touhoufan <touhoufan2024@gmail.com>

--- mypath/scripts/getData.sh
+++ mypath/scripts/getData.sh
@@ -6,7 +6,7 @@ urlB="https://hk.gh-proxy.org"
 urlC="https://cdn.gh-proxy.org"
 urlD="https://edgeone.gh-proxy.org"
 
-wget "${urlC}/https://github.com/chenditc/investment_data/releases/latest/download/qlib_bin.tar.gz" -O /tmp/qlib_bin.tar.gz
+wget --no-proxy "${urlA}/https://github.com/chenditc/investment_data/releases/latest/download/qlib_bin.tar.gz" -O /tmp/qlib_bin.tar.gz
 mv  ~/.qlib/qlib_data/cn_data  ~/.qlib/qlib_data/cn_data_back_$(date +%Y%m%d_%H%M%S)
 mkdir -p ~/.qlib/qlib_data/cn_data
 tar -zxvf /tmp/qlib_bin.tar.gz -C ~/.qlib/qlib_data/cn_data --strip-components=1
